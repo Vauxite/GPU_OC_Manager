4625267ff1808bbad233347be92261a36326db02
Vauxite <Vauxite@users.noreply.github.com>
2017-12-04 23:16:23 +0100

    Added a cookbook

---
+++ cookbooks/TimeBased.ps1
@@ -0,0 +1,61 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$StartDates,
+    [Parameter(Mandatory=$true)]
+    [string[]]$EndDates,
+    [Parameter(Mandatory=$true)]
+    [int[]]$ABProfile
+
+)
+
+##Example config
+#
+# $StartTime = "07:40","22:00","00:00"
+# $EndTime = "22:00","23:59","07:40"
+# $ABProfile = 5,1,1
+#
+##End Example config
+
+
+$OCmgrLoc = "D:\Programs\powershell\GPU_OC_Manager\GPU_OC_Manager.ps1"
+
+
+
+function inTime{
+    param(
+        [string]$StartTimeRange,
+        [string]$EndTimeRange,
+        [string]$NowTimeRange
+    )
+    $min = Get-Date $StartTimeRange
+    $max = Get-Date $EndTimeRange
+    $now = Get-Date $NowTimeRange
+
+    if ($min.TimeOfDay -le $now.TimeOfDay -and $max.TimeOfDay -ge $now.TimeOfDay) {
+        return $TRUE
+    }else{
+        return $FALSE
+    }
+
+}
+
+
+
+#Transitive property of equality. Make sure all arrays are of equal length.
+if(($StartTime.Length -eq $EndTime.Length) -and ($StartTime.Length -eq $Profile.Length)){
+    $now = Get-Date -Format HH:mm
+    $arrC = 0
+    foreach($range in $StartTime){
+        if(inTime -StartTimeRange $StartTime[$arrC] -EndTimeRange $EndTime[$arrC] -NowTimeRange $now){
+            Write-Host "Using profile"$ABProfile[$arrC]
+            &$OCmgrLoc -profile $ABProfile[$arrC]
+        }
+        $arrC++
+
+     }
+
+
+
+}
+
+
